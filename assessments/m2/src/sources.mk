24e51e7cb17af9cfd194b54e5bbb6bc724fc1493
vakeesank99 <vakeesank99@gmail.com>
2025-03-04 19:38:40 +0530

    assignmnet 2 is completed

--- assessments/m2/src/sources.mk
+++ assessments/m2/src/sources.mk
@@ -8,10 +8,15 @@
 # misuse of this material. 
 #
 #*****************************************************************************
-
+CURDIR=$(PWD)
 # Add your Source files to this variable
-SOURCES =
+SOURCES = 	memory.c \
+			main.c 
+#system_msp432p401r.c \ interrupts_msp432p401r_gcc.c \ startup_msp432p401r_gcc.c \
+
 
 # Add your include paths to this variable
-INCLUDES = 
+INCLUDES = -I../include/common/ \
+			-I../include/msp432/ \
+			-I../include/CMSIS/ 
 
