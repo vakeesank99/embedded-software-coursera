7419ea74308bae985363a2c1630e25d5438315e0
vakeesank99 <vakeesank99@gmail.com>
2025-03-18 19:46:57 +0530

    my_itoa added

--- course1/src/data.c
+++ course1/src/data.c
@@ -12,6 +12,17 @@
  * Example my_itoa(ptr, 1234, 10) should return an ASCII string length of 5 (including the null terminator).
  * This function needs to handle signed data. 
  * You may not use any string functions or libraries
+ * 
+ * example: 
+ *     uint8_t *ptr;
+    uint8_t len=my_itoa(-16,ptr,2 );
+    //  printf("%i\n",len);
+    uint8_t i=0;
+    while(i<len){
+            printf(" %c ",*(ptr+i));
+            i++;
+    }
+ * 
 */
 uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base){
     uint8_t i=0,len=0;
@@ -47,4 +58,70 @@ uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base){
     
     return len;
 }
+/**
+ * ASCII-to-Integer needs to convert data back from an ASCII represented string into an integer type.
+ * All operations need to be performed using pointer arithmetic, not array indexing
+ * The character string to convert is passed in as a uint8_t * pointer (ptr).
+ * The number of digits in your character set is passed in as a uint8_t integer (digits).
+ * You should be able to support bases 2 to 16. The converted 32-bit signed integer should be returned.
+ * This function needs to handle signed data. You may not use any string functions or libraries
+ * 
+ * example:
+ *     uint8_t *ptr;
+    uint8_t base=16;
+    int32_t data=-10;
+    uint8_t len=my_itoa(data,ptr,base );
+    //  printf("%i\n",len);
+    // uint8_t i=0;
+    // while(i<len){
+    //         printf(" %c ",*(ptr+i));
+    //         i++;
+    // }
+    int32_t num=my_atoi(ptr,len,base);
+    printf("%i",num);
+    
+ * 
+*/
+int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base){
+    uint8_t i=0;
+    int32_t num=0;
+    uint32_t temp_base=1;
+    int8_t neg=1;
+    if (*ptr==(uint8_t)(45)){
+        neg =-1;
+        i++;
+        while(i<digits){
+            if (*(ptr+digits-i) >55){
+                num = (*(ptr+digits-i)-55)* temp_base + num;
+            } 
+            else{
+                num = (*(ptr+digits-i)-48)* temp_base + num;
+                
+            }
+            
+            temp_base=temp_base*base;
+            i++;
+        }
+                
+                
+    }
+    else{
+        while(i<digits){
+        if (*(ptr+digits-i) >55){
+                num = (*(ptr+digits-i-1)-55)* temp_base + num;
+            } 
+            else{
+                num = (*(ptr+digits-i-1)-48)* temp_base + num;
+                
+            }
+            temp_base=temp_base*base;
+            i++;
+        }
+    }
+    
+    
+    return (int32_t)neg*num;
+}
+
+
 
